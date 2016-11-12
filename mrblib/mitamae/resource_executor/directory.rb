068ce2d1e9bc9f450dfbcfcd2cc8f15d288f98ad
Takashi Kokubun <takashikkbn@gmail.com>
2016-11-12 15:49:41 -0500

    Revert "Utilize mruby-file-stat" to build easily
    This reverts commit 2900722323289fe9c6d7ffb06d6c21599b9d0d75.

--- mrblib/mitamae/resource_executor/directory.rb
+++ mrblib/mitamae/resource_executor/directory.rb
@@ -13,7 +13,7 @@ module MItamae
             run_specinfra(:change_file_owner, desired.path, desired.owner, desired.group)
           end
         else
-          if FileTest.directory?(desired.path)
+          if run_specinfra(:check_file_is_directory, desired.path)
             run_specinfra(:remove_file, desired.path)
           end
         end
